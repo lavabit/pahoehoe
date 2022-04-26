49eb7171568f99eb3121ae9a96bb55ccfafbdbd7
Ladar Levison <ladar@lavabit.com>
2022-04-25 19:25:00 -0500

    Error check the sftp step.

--- run.sh
+++ run.sh
@@ -117,11 +117,14 @@ vagrant ssh-config debian_build > $BASE/build/config
 
 # If there is an output folder, extract the development APKs from the build environment.
 vagrant ssh -c "test -d \$HOME/android/app/build/outputs/" debian_build &> /dev/null && \
-{ printf "cd /home/vagrant/android/app/build/\nlcd $BASE/build/\nget -r outputs\n" | sftp -F $BASE/build/config debian_build 1>/dev/null ; }
+ printf "cd /home/vagrant/android/app/build/\nlcd $BASE/build/\nget outputs\n" | sftp -F $BASE/build/config debian_build 1>/dev/null || \
+ { RESULT=$? ; tput setaf 1 ; printf "Outputs download error. [ OUTPUTS = $RESULT ]\n\n" ; tput sgr0 ; exit 1 ; }
+
 
 # If there is a releases folder, extract the signed release APKs files from the build environment.
 vagrant ssh -c "test -d \$HOME/android/releases/" debian_build &> /dev/null && \
-{ printf "cd /home/vagrant/android/\nlcd $BASE/build/\nget -r releases\n" | sftp -F $BASE/build/config debian_build 1>/dev/null ; }
+  { printf "cd /home/vagrant/android/\nlcd $BASE/build/\nget releases\n" | sftp -F $BASE/build/config debian_build 1>/dev/null || \
+  { RESULT=$? ; tput setaf 1 ; printf "Outputs download error. [ OUTPUTS = $RESULT ]\n\n" ; tput sgr0 ; exit 1 ; }}
 
 # Termux version 118 requires at least v24 of the Android SDK.
 [ -d $BASE/build/termux/ ] && rm --force --recursive $BASE/build/termux/ ; mkdir --parents $BASE/build/termux/
